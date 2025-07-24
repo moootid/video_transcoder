184b8461c6e44a173f1132bb7dd6af947e017763
Mohammed Almalki <mr.motid@gmail.com>
2025-07-24 17:40:28 +0300

    Initial commit

---
+++ lib/video_transcoder.ex
@@ -0,0 +1,18 @@
+defmodule VideoTranscoder do
+  @moduledoc """
+  Documentation for `VideoTranscoder`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> VideoTranscoder.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/video_transcoder_test.exs
@@ -0,0 +1,8 @@
+defmodule VideoTranscoderTest do
+  use ExUnit.Case
+  doctest VideoTranscoder
+
+  test "greets the world" do
+    assert VideoTranscoder.hello() == :world
+  end
+end
